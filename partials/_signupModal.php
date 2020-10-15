c1aa315cb71e571c99c340901c9926068d9a21c2
Vikas Hirave <45948809+vikashirave@users.noreply.github.com>
2020-10-15 23:28:37 +0530

    commit

--- partials/_signupModal.php
+++ partials/_signupModal.php
@@ -13,7 +13,7 @@
                 
                     <div class="form-group">
                         <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <input type="email"  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
                             else.</small>
                     </div>
